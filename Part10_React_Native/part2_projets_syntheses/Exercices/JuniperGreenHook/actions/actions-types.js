6918646cad6e9d4f677ac153198abe618356be60
Antoine07 <antoine.lucsko@gmail.com>
2020-07-22 17:17:43 +0200

    algo dans actions-types

--- Part10_React_Native/part2_projets_syntheses/Exercices/JuniperGreenHook/actions/actions-types.js
+++ Part10_React_Native/part2_projets_syntheses/Exercices/JuniperGreenHook/actions/actions-types.js
@@ -1,18 +1,32 @@
 
 export const MAX_JUNIPER = 100;
 
-export const possibleMultiple = (n, possibles = new Map([])) => {
+// possibles est un tableau dans lequel je vais placer les valeurs que l'on aura déjà choisie 
+export const possibleMultiples = (n, possibles = new Map([])) => {
 
-    let multiple = new Map([]);
+    let multiples = new Map([]);
     let j = 1, num = n;
 
-    while ( num <= MAX_JUNIPER ) {
+    while (num <= MAX_JUNIPER) {
         num = j * n;
-        if ( num <= MAX_JUNIPER  && possibles.has(num) === false) multiple.set(num, num);
+        if (num <= MAX_JUNIPER && possibles.has(num) === false) multiples.set(num, num);
 
         j++;
     }
 
+    return multiples;
+}
 
-    return multiple;
+export const possibleDivisors = (n, possibles = new Map([])) => {
+
+    let divisors = new Map([]);
+    let d = 2;
+
+    while (d <= n) {
+        if (n % d === 0 && possibles.has(d) === false) divisors.set(d, d);
+
+        d++;
+    }
+
+    return divisors;
 }
