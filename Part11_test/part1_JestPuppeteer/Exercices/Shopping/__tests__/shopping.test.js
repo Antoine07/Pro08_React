c642ed173f62d481d991bb2d010703368f1739d9
Antoine07 <antoine.lucsko@gmail.com>
2020-07-24 15:53:56 +0200

    shopping list

--- Part11_test/part1_JestPuppeteer/Exercices/Shopping/__tests__/shopping.test.js
+++ Part11_test/part1_JestPuppeteer/Exercices/Shopping/__tests__/shopping.test.js
@@ -15,21 +15,40 @@ describe("test Shopping", () => {
     for (let p of shoppingList) {
       if (p['price'] != undefined && p['count'] != undefined && p['total'] != undefined) {
         total = p['price'] * p['count'];
-
-        expect(total).toEqual( Number(p['total']) );
+        expect(total).toEqual(Number(p['total']));
       }
     }
   });
 
-  // total dans le shopping list
   test('Test total is not good', () => {
     for (let p of shoppingList) {
       if (p['total'] != undefined) { total += Number(p['total']); }
     }
-
     amount = shoppingList.pop().amount || undefined;
     if (amount != undefined) amount = Number(amount);
 
-    expect(amount).not.toEqual(total); // ce n'est pas le même total
+    expect(amount).not.toEqual(total);
+
   });
+
+
+  // 3
+  test('Test pattern tags ', () => {
+    for (let p of shoppingList) {
+      if (p['tags'] != undefined && Array.isArray(p['tags'])) {
+        // new Set permet de ne pas avoir de doublon mais on aurit pu directement passer p['tags'] le tableau lui-même
+        expect(new Set(p['tags'])).toContain('love');
+      }
+    }
+  });
+
+  // 4
+  test('Test pattern tags no war ', () => {
+    for (let p of shoppingList) {
+      if (p['tags'] != undefined && Array.isArray(p['tags'])) {
+        expect(new Set(p['tags'])).not.toContain('war');
+      }
+    }
+  });
+
 });
